b83e55f8dac157ed8514dffc2365a00158693be1
DavidB09 <davcoding1@gmail.com>
2024-10-12 23:10:19 -0500

    add amplify init files

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import prisma from '../db/prisma.ts';
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app: express.Application = express();
+const port = 3000;
+
+app.listen(port, async () => {
+    console.log(`App listening on port ${port}`);
+});
+
+// Homepage
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send("Hello World!");
+});
+
