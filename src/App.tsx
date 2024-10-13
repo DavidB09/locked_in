bebefa78ec3d976f0f9c3e69d2e9c8d9508ed9b3
crlinpost <lincrystalhome@gmail.com>
2024-10-12 20:35:44 -0500

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import SignIn from "./components/SignIn"
+import Dashboard from "./pages/Dashboard";
+
 
 
 export default function App() {
 
   return (
   <>
-  <SignIn/>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<SignIn/>}></Route>
+        <Route path="home" element={<Dashboard/>}></Route>
+      </Routes>
+      </BrowserRouter>
 
   </>
   )
