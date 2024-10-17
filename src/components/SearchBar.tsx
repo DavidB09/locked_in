e567ab90ad752dfd6a234f34ec6cb416a7379055
crlinpost <lincrystalhome@gmail.com>
2024-10-17 14:56:25 -0500

    Resolved type issues with props.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,18 @@
 import { IconButton, InputBase, Paper } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
-import { Dispatch } from "react";
+import { Dispatch, SetStateAction } from "react";
 
+interface Sprops {
+    setSearch : Dispatch<SetStateAction<string>>
+}
 
-export default function Search (setSearch : Dispatch<string>){
+export default function Search (props :  Sprops){
 
     return (
         <Paper component="form"
         sx={{ margin: "2vh 2vh", alignItems: 'center', width: '20vw'}}>
             <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search Websites"
-            onInput={(e) => setSearch((e.target as HTMLTextAreaElement).value)}
+            onInput={(e) => props.setSearch((e.target as HTMLTextAreaElement).value)}
             />
             <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
                 <SearchIcon />
