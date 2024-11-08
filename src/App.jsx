00062f8e0b376c8e73c269eeb4d30d20562de27e
crlinpost <lincrystalhome@gmail.com>
2024-11-08 15:29:12 -0600

    Brush up sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
@@ -95,8 +94,63 @@ export default function App() {
     }
   });
 
+  const components = {
+    Header() {
+  
+      return (
+        <div  style={{textAlign: "center"}}>
+          <img
+            alt="Our"
+            src="/src/assets/logol.png"
+            style={{width: "50%", height: "50%"}}
+          />
+        </div>
+      );
+    },
+  }
+
+  //   const { tokens } = useTheme();
+  // const theme = {
+  //   name: 'Auth Example Theme',
+  //   tokens: {
+  //     components: {
+  //       authenticator: {
+  //         router: {
+  //           boxShadow: `0 0 16px ${tokens.colors.overlay['10']}`,
+  //           borderWidth: '0',
+  //         },
+  //         form: {
+  //           padding: `${tokens.space.medium} ${tokens.space.xl} ${tokens.space.medium}`,
+  //         },
+  //       },
+  //       button: {
+  //         primary: {
+  //           backgroundColor: tokens.colors.neutral['100'],
+  //         },
+  //         link: {
+  //           color: tokens.colors.purple['80'],
+  //         },
+  //       },
+  //       fieldcontrol: {
+  //         _focus: {
+  //           boxShadow: `0 0 0 2px ${tokens.colors.purple['60']}`,
+  //         },
+  //       },
+  //       tabs: {
+  //         item: {
+  //           color: tokens.colors.neutral['80'],
+  //           _active: {
+  //             borderColor: tokens.colors.neutral['100'],
+  //             color: tokens.colors.purple['100'],
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
+
   return (
-    <Authenticator>
+    <Authenticator components={components}>
       {({ signOut, user }) => (
         <ThemeProvider theme={theme}>
           <Dashboard user={user} signOut={signOut} />
